cb46674dbcd12390cf1b382b90aa99ddfe27f4e1
Hossein Akbari <itshossein128@gmail.com>
2024-12-05 15:33:11 +0330

    removed unused components

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { MdPreview } from "react-icons/md";
 import { FaCode } from "react-icons/fa";
 
