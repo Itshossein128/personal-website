0daabdf2d06c9ec6369611e2b34f2ff84b29a185
Hossein Akbari <itshossein128@gmail.com>
2024-12-09 17:10:03 +0330

    added placeholder for avatar image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="home-main flex flex-col lg:flex-row items-center sm:justify-center lg:justify-between text-center lg:text-left py-10 gap-8 lg:gap-10 xl:gap-32 lg:pr-10 xl:pr-20">
-      <Image
-        alt=""
-        src={"/avatar2.jpg"}
-        width={700}
-        height={700}
-        className="aspect-square rounded-sm max-w-96 md:max-w-sm lg:max-w-lg xl:max-w-xl w-full shrink-0"
-      />
+      <div className="aspect-square rounded-sm max-w-96 md:max-w-sm lg:max-w-lg xl:max-w-xl w-full shrink-0 overflow-hidden placeholder-animation relative">
+        <Image alt="" src={"/avatar2.jpg"} width={700} height={700} />
+      </div>
       <div className="max-w-2xl">
         <div className="pl-10 mb-5">
           <span className="first-char-dash relative font-bold text-4xl text-primary inline-block mb-1">
